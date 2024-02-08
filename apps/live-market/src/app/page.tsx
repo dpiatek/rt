b261d66bca344bede9db1e6b7b042d649b4a143e
Dominik Piatek <do.piatek@gmail.com>
2024-02-08 14:58:30 +0000

    Add basic HTML

--- apps/live-market/src/app/page.tsx
+++ apps/live-market/src/app/page.tsx
@@ -1,5 +1,57 @@
 export default async function Index() {
   return (
-    <p className="p-10">page</p>
+    <main>
+      <section>
+        <div>
+          <h2>Accepted Orders</h2>
+          <button>Collapse</button>
+        </div>
+
+        <div>
+          <div>
+            <div>Quality</div>
+            <div>Dispatch date</div>
+            <div>Quantity</div>
+            <div>Price / kg</div>
+          </div>
+
+          <div>
+            <div>
+              <div>
+                <span>Accepted</span>
+                <p>Salmon machine cut filet</p>
+                <span>just now</span>
+              </div>
+
+              <div>
+                <span>100+g</span>
+                <span>•</span>
+                <span>whole</span>
+                <span>•</span>
+                <span>Trawls</span>
+                <span>•</span>
+                <span>FRA</span>
+              </div>
+            </div>
+
+            <div>
+              <span>A-</span>
+            </div>
+
+            <div>
+              <span>Today</span>
+            </div>
+
+            <div>
+              <span>&lt;100 boxes</span>
+            </div>
+
+            <div>
+              <span>€7.00</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
